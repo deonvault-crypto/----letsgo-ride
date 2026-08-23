780171cb73b5b9d42eba6203ece4900445deac72
deonvault-crypto <deonvault@gmail.com>
2026-08-23 13:34:23 +0200

    refactor(customer): remove legacy passenger results redirect

--- mobile/app/(passenger)/results.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect, useLocalSearchParams } from "expo-router";
-
-export default function LegacyPassengerResultsRedirect() {
-  const params = useLocalSearchParams<Record<string, string>>();
-  return <Redirect href={{ pathname: "/(customer)/results", params } as never} />;
-}
