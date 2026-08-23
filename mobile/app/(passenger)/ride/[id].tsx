02a541820660a924405798eae70631fc5019ccfd
deonvault-crypto <deonvault@gmail.com>
2026-08-23 13:35:17 +0200

    refactor(customer): remove legacy passenger ride redirect

--- mobile/app/(passenger)/ride/[id].tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect, useLocalSearchParams } from "expo-router";
-
-export default function LegacyPassengerRideRedirect() {
-  const { id } = useLocalSearchParams<{ id: string }>();
-  return <Redirect href={`/(customer)/ride/${id}` as never} />;
-}
