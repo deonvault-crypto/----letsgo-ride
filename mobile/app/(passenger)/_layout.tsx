dfa35a19e0b6a2339ec04b17735bcf7dcb62484a
deonvault-crypto <deonvault@gmail.com>
2026-08-23 13:33:49 +0200

    refactor(customer): remove legacy passenger route shell

--- mobile/app/(passenger)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function PassengerLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false, animation: "slide_from_right" }}>
-      <Stack.Screen name="home" options={{ gestureEnabled: false, animation: "fade" }} />
-    </Stack>
-  );
-}
