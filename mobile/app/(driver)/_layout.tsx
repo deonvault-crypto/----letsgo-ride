aee257b1e40426bae1e1a06c3da07648a3bb3761
deonvault-crypto <deonvault@gmail.com>
2026-08-30 01:33:05 +0200

    feat(mobile): mount driver location sync

--- mobile/app/(driver)/_layout.tsx
+++ mobile/app/(driver)/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
 
+import { HailingDriverLocationSync } from "../../components/hailing/HailingDriverLocationSync";
 import { DriverWorkspaceProvider } from "../../contexts/DriverWorkspaceContext";
 
 export default function DriverLayout() {
   return (
     <DriverWorkspaceProvider>
+      <HailingDriverLocationSync />
       <Stack screenOptions={{ headerShown: false, animation: "slide_from_right", gestureEnabled: true }}>
         <Stack.Screen name="home" options={{ gestureEnabled: false, animation: "fade" }} />
       </Stack>
