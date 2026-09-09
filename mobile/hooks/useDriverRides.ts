2f2910d773ef7fff781a8c1bbcd9b9c36b831b4b
deonvault-crypto <deonvault@gmail.com>
2026-09-09 18:57:10 +0200

    Remove redundant Driver rides wrapper

--- mobile/hooks/useDriverRides.ts
+++
@@ -1,6 +0,0 @@
-import { useDriverWorkspace } from "../contexts/DriverWorkspaceContext";
-
-export function useDriverRides() {
-  const { rides, loading, error, reconcile } = useDriverWorkspace();
-  return { rides, loading, error, reload: reconcile };
-}
