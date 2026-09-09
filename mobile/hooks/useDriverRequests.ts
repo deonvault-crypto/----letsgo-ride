d912dd1e80347446f9d2393c3c5b3855b6d00f4c
deonvault-crypto <deonvault@gmail.com>
2026-09-09 18:29:44 +0200

    Remove unused Driver requests wrapper hook

--- mobile/hooks/useDriverRequests.ts
+++
@@ -1,6 +0,0 @@
-import { useDriverWorkspace } from "../contexts/DriverWorkspaceContext";
-
-export function useDriverRequests() {
-  const { requests, loading, error, reconcile } = useDriverWorkspace();
-  return { requests, loading, error, reload: reconcile };
-}
